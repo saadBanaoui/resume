24361f29937832e1718decacb0efb7db6bdc8c7f
Saad <se@kapt.mobi>
2018-01-29 16:23:34 +0100

    Feat : add Experiences component

---
+++ src/components/Experiences.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="experiences">
+  <p>{{ msg }}</p>
+</div>
+</template>
+<script>
+export default {
+  name: 'Experiences',
+  data () {
+    return {
+      msg: 'Welcome to my saas'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import About from '@/components/About'
+import Experiences from '@/components/Experiences'
 Vue.use(Router)
-
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    { path: '/', name: 'About', component: About },
+    { path: '/', name: 'Experiences', component: Experiences }
   ]
 })
