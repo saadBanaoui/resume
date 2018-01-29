2968b14d6d74715c874e0b0bafc19da4c43fd94d
Saad <se@kapt.mobi>
2018-01-29 17:47:32 +0100

    add new files

---
+++ src/components/Brief.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="brief">
+    <div class="avatar">
+      <EditImage :src="require('@/assets/logo.png')" width="212" height="212" :isCircle="true" class="img"></EditImage>
+      <div class="name">El banaoui Saad</div>
+      <div class="job">Webdesigner</div>
+    </div>
+  </div>
+</template>
+<script>
+  import EditImage from '@/components/edit-image'
+  export default {
+    name: 'Brief',
+    components: {
+      EditImage
+    }
+  }
+</script>
